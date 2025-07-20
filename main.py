05d54d6900b0055392112564f13246dd796a67c1
alok2608 <alokpatel2608@gmail.com>
2025-07-21 00:59:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from routers import products, orders
 
 app = FastAPI()
 
+
+@app.get("/")
+def read_root():
+    return {"message": "E-commerce API is live and running "}
+
 app.include_router(products.router)
 app.include_router(orders.router)
